ac9f9f3ea19d0c328cf1871fece45f1bc789cfdd
Pedro Henrique <pedroo.contato1@gmail.com>
2025-04-11 12:21:14 -0300

    Ajuste no tempo de carregamento do WebDriverWait

--- app/services/scrap.py
+++ app/services/scrap.py
@@ -12,19 +12,19 @@ def parser():
     try:        
         driver = configura_driver()
         driver.get("https://web.whatsapp.com")
-        tempo_carregamento.sleep(2)
+        tempo_carregamento.sleep(3)
         
-        elemento_caixa_pesquisa = WebDriverWait(driver, 2).until(EC.presence_of_element_located((By.CSS_SELECTOR, "p.selectable-text.copyable-text")))        
+        elemento_caixa_pesquisa = WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.CSS_SELECTOR, "p.selectable-text.copyable-text")))        
         elemento_caixa_pesquisa.send_keys("Financeiro")
         
-        tempo_carregamento.sleep(2)
+        tempo_carregamento.sleep(3)
         
-        elemeno_icone_grupo = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.CSS_SELECTOR, "span.matched-text")))
+        elemeno_icone_grupo = WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.CSS_SELECTOR, "span.matched-text")))
         elemeno_icone_grupo.click()
         
-        elemento_chat = WebDriverWait(driver, 2).until(EC.presence_of_element_located((By.CSS_SELECTOR, "div[class*='x1vjfegm x1cqoux5 x14yy4lh']"))        )
+        elemento_chat = WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.CSS_SELECTOR, "div[class*='x1vjfegm x1cqoux5 x14yy4lh']"))        )
         
-        tempo_carregamento.sleep(4)
+        tempo_carregamento.sleep(3)
         
         mensagens = elemento_chat.find_elements(By.CSS_SELECTOR, "div[class*='_akbu']")
         array_mgs = [row.text.split('\n') for row in mensagens]
