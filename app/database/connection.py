4d416cec3eedc39602f5303043b2872e44358920
Pedro Henrique <pedroo.contato1@gmail.com>
2025-04-16 13:54:57 -0300

    Atualiza conexão para usar variáveis de ambiente

--- app/database/connection.py
+++ app/database/connection.py
@@ -1,20 +1,22 @@
+import os
 import pyodbc
-from app.utils.logger import configura_log
+from dotenv import load_dotenv
+from app.utils.logger import log_builder
 
-log = configura_log("database.py")
+log = log_builder("database.py")
+load_dotenv()
 
 def database_connection():
-    server = 'DESKTOP-98I4FGO'
-    database = 'FINANCEIRO'
-    user = 'Admin'
-    password = '66tUa3ue'
+    server = os.getenv("DB_SERVER")
+    database = os.getenv("DB_DATABASE")
+    username = os.getenv("DB_USER")
+    password = os.getenv("DB_PASSWORD")
 
     try:
-        connection = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER=' + server + ';DATABASE=' + database + ';UID=' + user + ';PWD=' + password)
+        connection = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER=' + server + ';DATABASE=' + database + ';UID=' + username + ';PWD=' + password)
         cursor = connection.cursor()
         return connection, cursor
 
     except pyodbc.Error as e:        
         log.error(f"Falha ao conectar ao banco de dados local: {e}")
         return None
-
